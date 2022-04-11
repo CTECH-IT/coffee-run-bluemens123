25f65078e39e4a934327d096c3819db9e1599233
Maximilian Comfere <7470101@isd535.org>
2022-04-11 09:32:38 -0500

    add, get, getAll, remove

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -8,11 +8,27 @@
         }
         this.serverUrl = url; 
     }
-    RemoteDataStore.prototype.add = function(key, val) {
-        $.post(this.serverUrl, val, function(serverResponse) {
+    RemoteDataStore.prototype.add = function (key, val) {
+        $.post(this.serverUrl, val, function (serverResponse) {
             console.log(serverResponse); 
         }); 
     }; 
+    RemoteDataStore.prototype.getAll = function(cb){
+        $.get(this.serverUrl, function (serverResponse) {
+            console.log(serverResponse); 
+            cb(serverResponse); 
+        }); 
+    };
+    RemoteDataStore.prototype.get = function (key, cb) {
+        $.get(this.serverUrl + '?emailAddress=' + key, function (serverResponse) {
+            console.log(serverResponse); 
+            cb(serverResponse); 
+        }); 
+    }; 
+    RemoteDataStore.prototype.remove = function (key) {
+        $.ajax(this.serverUrl + '?emailAddress=' + key, { type: 'DELETE' }); 
+    }; 
+    
     App.RemoteDataStore = RemoteDataStore;
     window.App = App; 
 })(window); 
